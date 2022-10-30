eb8c78936c3699cb1cffb9e92313115d2c06500c
Lachlan Campbell <35567774+Doxxus@users.noreply.github.com>
2022-10-30 17:30:26 -0400

    Initial Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			colors: {
+				'muddy-violet': '#302D3E',
+				'translucent-violet': '#DAD5EA',
+			},
+		},
+	},
+
+	plugins: []
+};
+
+module.exports = config;
