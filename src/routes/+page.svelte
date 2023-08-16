dc278bbff690f7bd8e0f55b739fef91c220aa4ff
Lachlan Campbell <35567774+Doxxus@users.noreply.github.com>
2023-08-16 15:40:42 -0400

    Implemented frameworks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,10 @@
 
     import * as info from '../json/info.json';
 
-    onMount(Intro.AnimateIntro);
+    onMount(() => { 
+        document.title = "Doxxus Portfolio";
+        Intro.AnimateIntro();
+    });
 
     let age: string = "";
     setInterval(() => {
@@ -22,8 +25,8 @@
     let show_experience: boolean = false;
     let show_skillset: boolean = false;
     let show_projects: boolean = false;
-    let show_education: boolean = false;
     let show_contact: boolean = false;
+    let show_links: boolean = false;
 
     let init_skillset: boolean = false;
 
@@ -112,21 +115,21 @@
             <p class="text-zinc-800 dark:text-zinc-200 tight">I'm currently interested in learning the SvelteKit framework and associated TypeScript web stack.</p>
         </div>
         <div class="center">
-            <div id="Experience" class="info_base fade_in" class:expand={show_experience} bind:this={Elements.eleExperience} on:click={() => {show_experience = ToggleExpand(show_experience)}} on:keydown={() => {show_experience = ToggleExpand(show_experience)}}>
+            <div id="Experience" class="info_base fade_in" class:expand={show_experience} style="--expand_height: 520px;" bind:this={Elements.eleExperience} on:click={() => {show_experience = ToggleExpand(show_experience)}} on:keydown={() => {show_experience = ToggleExpand(show_experience)}}>
                 <span class="sub_heading">Experience</span>
             </div>
-            <div id="Languages" class="info_base fade_in" class:expand={show_skillset} bind:this={Elements.eleLanguages} on:mouseenter={() => {init_skillset = true}} on:mouseleave={() => {init_skillset = false}} on:click={() => {show_skillset = ToggleExpand(show_skillset)}} on:keydown={() => {show_skillset = ToggleExpand(show_skillset)}}>
+            <div id="Languages" class="info_base fade_in" class:expand={show_skillset} style="--expand_height: {(info.languages.length * 40) + 50}px;" bind:this={Elements.eleLanguages} on:mouseenter={() => {init_skillset = true}} on:mouseleave={() => {init_skillset = false}} on:click={() => {show_skillset = ToggleExpand(show_skillset)}} on:keydown={() => {show_skillset = ToggleExpand(show_skillset)}}>
                 <span class="sub_heading">Skills</span>
                 <Skillset languages='{info.languages}' show_skillset='{show_skillset}' init_skillset='{init_skillset}'></Skillset>
             </div>
-            <div id="Projects" class="info_base fade_in" class:expand={show_projects} bind:this={Elements.eleProjects} on:click={() => {show_projects = ToggleExpand(show_projects)}} on:keydown={() => {show_projects = ToggleExpand(show_projects)}}>
+            <div id="Projects" class="info_base fade_in" class:expand={show_projects} style="--expand_height: 520px;" bind:this={Elements.eleProjects} on:click={() => {show_projects = ToggleExpand(show_projects)}} on:keydown={() => {show_projects = ToggleExpand(show_projects)}}>
                 <span class="sub_heading">Projects</span>
             </div>
-            <div id="Education" class="info_base fade_in" class:expand={show_education} bind:this={Elements.eleEducation} on:click={() => {show_education = ToggleExpand(show_education)}} on:keydown={() => {show_education = ToggleExpand(show_education)}}>
-                <span class="sub_heading">Education</span>
-            </div>
-            <div id="Contact" class="info_base fade_in" class:expand={show_contact} bind:this={Elements.eleContact} on:click={() => {show_contact = ToggleExpand(show_contact)}} on:keydown={() => {show_contact = ToggleExpand(show_contact)}}>
+            <div id="Contact" class="info_base fade_in" class:expand={show_contact} style="--expand_height: 520px;" bind:this={Elements.eleContact} on:click={() => {show_contact = ToggleExpand(show_contact)}} on:keydown={() => {show_contact = ToggleExpand(show_contact)}}>
                 <span class="sub_heading">Contact</span>
+            </div>
+            <div id="Links" class="info_base fade_in" class:expand={show_links} style="--expand_height: 300px;" bind:this={Elements.eleEducation} on:click={() => {show_links = ToggleExpand(show_links)}} on:keydown={() => {show_links = ToggleExpand(show_links)}}>
+                <span class="sub_heading">Links</span>
             </div>
         </div>
     </div>
@@ -134,7 +137,7 @@
 
 <style lang="scss">
     .expand {
-        height: 500px;
+        height: var(--expand_height);
         border-width: 2px;
     }
 </style>
--- src/routes/skillset.svelte
+++ src/routes/skillset.svelte
@@ -4,7 +4,11 @@
     export let init_skillset: boolean;
     
     interface framework {
-
+        id: number,
+        name: string,
+        expand: boolean,
+        image_path: string,
+        about: string
     }
     
     interface language {
@@ -12,6 +16,7 @@
         name: string,
         proficiency: number,
         expand: boolean,
+        image_path: string,
         about: string,
         frameworks: Array<framework>
     }
@@ -33,15 +38,54 @@
 
     <div class="lang_proficiencies">
         {#each languages as lang}
-            <div class="lang_prof" class:no_delay={no_delay} class:expand={lang.expand} class:show={show_skillset || init_skillset} style="--width: {lang.proficiency * 0.98}%; --delay: {lang.id * 50}ms; --top: {lang.id * 35 + 80}px" on:click={() => {lang.expand = !lang.expand; FixDelay();}} on:keydown={() => {lang.expand = !lang.expand}}>
+            <div class="lang_prof" class:no_delay={no_delay} class:expand={lang.expand} class:show={show_skillset || init_skillset} style="--width: {lang.proficiency * 0.98}%; --delay: {lang.id * 50}ms; --top: {lang.id * 35 + 80}px; --anim_delay: {Math.random() * 6000}ms;" on:click|stopPropagation={() => {lang.expand = !lang.expand; FixDelay();}} on:keydown={() => {lang.expand = !lang.expand}}>               
                 <span class="lang_name" class:no_delay={no_delay} class:expand={lang.expand} class:show={show_skillset || init_skillset} style="--delay: {lang.id * 50}ms;">{lang.name}</span>
-                <div class="lang_about" class:show={lang.expand}>{lang.about}</div>
+                <div class="lang_info_container" class:show={lang.expand}>
+                    <div class="lang_logo"><img src='{lang.image_path}' alt=''/></div>
+                    <div class="lang_about"><span>{lang.about}</span></div>
+                    {#if lang.frameworks.length == 0}
+                        <div class="no_frameworks">
+                            <span>No additional frameworks, libraries, or additional technologies here, just raw {lang.name}!</span>
+                        </div>
+                    {:else}
+                        <div class="frameworks_title">Frameworks / Libraries / Related Technologies</div>
+                        <div class="frameworks_container">
+                            {#each lang.frameworks as fw}
+                                <div class="framework" style="--padding: {Math.max(18 - (lang.frameworks.length) * 2, 2)}%;">
+                                    <img src='{fw.image_path}' alt=''/>
+                                </div>
+                            {/each}
+                        </div>
+                    {/if}
+                </div>              
             </div>
         {/each}
     </div>
 </main>
 
 <style lang="scss">
+    .lang_info_container {
+        display: grid;
+        grid-template-columns: [col1] 45% [col2] 10% [col3] 45% [col4] auto;
+        grid-template-rows: [row0] 30px [row1] 120px [row2] 220px [row3] auto;
+        place-items: center;
+        transition: opacity 0.2s ease;
+        transition-delay: 0s;
+        opacity: 0;
+    }
+
+    .lang_info_container.show {
+        opacity: 1;
+        transition-delay: 0.7s;
+    }
+
+    .lang_logo {
+        object-fit: cover;
+        width: 20%;
+        grid-column: col1 / col2;
+        grid-row: row0 / row2;
+    }
+
     .lang_proficiencies {
         @apply text-zinc-800 dark:text-zinc-200;
         font-family: Helvetica;       
@@ -75,6 +119,7 @@
         background: linear-gradient(-45deg, #a07dc0, #8f5acc, #6d41be, #5f43df, #4927df);
         background-size: 400% 400%;
         animation: gradient 15s ease infinite;
+        animation-delay: var(--anim_delay);
         opacity: 0.7;
         z-index: 5;
     }
@@ -91,8 +136,10 @@
         padding-top: 3px;
         margin-bottom: 1px;
         padding-right: 20px;
+        width: 100%;
         
         right: 0%;
+        transition: 200ms transform;
 
         opacity: 0;
         transition: opacity 0.2s ease;
@@ -108,7 +155,7 @@
     .lang_prof.expand {
         position: absolute;
         top: 50px;
-        height: 400px;
+        height: 450px;
         width: 90% !important;
         left: 5%;
         border-radius: 20px 20px 20px 20px;
@@ -116,26 +163,55 @@
         opacity: 1;
     }
 
-    .lang_name.expand {       
+    .lang_name.expand {
         right: 45%;
         font-size: 2em;
     }
 
     .lang_about {
+        grid-column: col1 / col2;
+        grid-row: row2 / row3;
         pointer-events: none;
-        position: absolute;
-        width: 50%;
-        padding: 10%;
+        padding-left: 50px;
+        padding-right: 50px;
+        padding-top: 10px;
         height: fit-content;
         text-align: center;
-        opacity: 0;
-        transition: opacity 0.2s ease;
-        transition-delay: 0s;
     }
 
-    .lang_about.show {
-        opacity: 1;
-        transition-delay: 0.7s;
+    .frameworks_title {
+        width: 100%;
+        font-size: 1.2em;
+        text-align: center;
+        grid-column: col3 / col4;
+        grid-row: row0 / row1;
+    }
+
+    .frameworks_container {
+        width: 100%;
+        grid-column: col3 / col4;
+        grid-row: row1 / row3;
+
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+        grid-template-rows: repeat(auto-fit, minmax(100px, 1fr));
+        place-items: center;
+
+        padding: 10px;
+    }
+
+    .framework {
+        position: relative;
+        padding: var(--padding);
+    }
+
+    .no_frameworks {
+        grid-column: col3 / col4;
+        grid-row: row0 / row3;
+
+        text-align: center;
+        font-size: 2em;
+        padding: 50px;
     }
 
     .no_delay {
